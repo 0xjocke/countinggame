1f215485639b5b2e9d2fd941f766b673df612ed9
Joachim <joachim.bachstatter@gmail.com>
2014-02-23 22:39:36 +0100

    config ex

---
+++ config-example.php
@@ -0,0 +1,30 @@
+<?php 
+	session_start();
+
+date_default_timezone_set('Europe/Stockholm');
+
+// DB settings
+define('DB_HOST', 'ADD');
+define('DB_NAME', 'ADD');
+define('DB_USER', 'ADD');
+define('DB_PASS', 'ADD');
+
+// Create connection with PDO
+$dbh = new PDO(
+  'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
+  DB_USER,
+  DB_PASS,
+  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
+);
+
+// Models
+require_once 'models/Person.php';
+require_once 'models/Authorization.php';
+
+
+
+// libs
+
+Person::setDbh($dbh);
+
+?>
