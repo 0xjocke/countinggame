bfb944a2e4024a0a6a5fac16c8c96957d7be850f
Joachim <joachim.bachstatter@gmail.com>
2014-02-26 11:51:13 +0100

    timer correct

--- public_html/js/build/script.js
+++ public_html/js/build/script.js
@@ -119,7 +119,7 @@ Game.prototype.points = function(){
 	$('#points').html(this.pointsCounter);
 };
 Game.prototype.timer = function(){
-	var count = 6;
+	var count = 60;
 	var timer = setInterval(function() {
 		$('#counter').html(count--);
 		if (count === 9) {
