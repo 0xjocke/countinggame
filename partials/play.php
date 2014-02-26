efe2bed8cdd28454792b8b4879b572a791dad7ec
Joachim <joachim.bachstatter@gmail.com>
2014-02-26 11:05:10 +0100

    mp3 backup, new bonus, 60 default

--- partials/play.php
+++ partials/play.php
@@ -2,7 +2,7 @@
 	<div class="container relative">
 		<div class="col3 playmode">
 			<p>Tid kvar:</p>
-			<p id="counter" class="large"></p>
+			<p id="counter" class="large">60</p>
 		</div>
 		<div class="col6 playmode">
 			<p id="mathQuestion" class="megalarge"></p>
@@ -19,7 +19,13 @@
 		<button id="answer3" class="answer"></button>
 		<button id="answer4" class="answer"></button>
 	</div>
-	<audio id="bonusPling" src="audio/pling.wav"></audio>
-	<audio id="clockSound" src="audio/clock.wav"></audio>
+	<audio id="bonusPling" src="audio/pling.wav">
+		<source src="audio/pling.wav" type="audio/wav" />
+		<source src="audio/pling.mp3" type="audio/mpeg" />
+	</audio>
+	<audio id="clockSound" src="audio/clock.wav">
+		<source src="audio/clock.wav" type="audio/wav" />
+		<source src="audio/clock.mp3" type="audio/mpeg" />
+	</audio>
 	<button id="dataloaded" class="hidden"></button>
 </section>
