5253de175912b6709c66d27812ce7616fe01e887
Joachim <joachim.bachstatter@gmail.com>
2014-02-26 10:31:09 +0100

    backend active, button font correct

--- public_html/backend.php
+++ public_html/backend.php
@@ -1,18 +1,18 @@
 <?php 
 	require_once '../config.php';
 
-	// if (isset($_POST['email'])) {
-	// 		$person = new Person($_POST);
-	// 		$person->save_to_db();
-	// 		$person->save_to_session();
-	// }
+	if (isset($_POST['email'])) {
+			$person = new Person($_POST);
+			$person->save_to_db();
+			$person->save_to_session();
+	}
 
-	// if (isset($_POST['points'])) {
-	// 	$playing_person = Person::find($_SESSION['id']);
-	// 	if ($playing_person->highscore < $_POST['points']) {
-	// 		$playing_person::save_highscore($_SESSION['id'], $_POST['points']);
-	// 	}
-	// }
+	if (isset($_POST['points'])) {
+		$playing_person = Person::find($_SESSION['id']);
+		if ($playing_person->highscore < $_POST['points']) {
+			$playing_person::save_highscore($_SESSION['id'], $_POST['points']);
+		}
+	}
 	if (isset($_GET['toplist'])) {
 		$table ='<table class="toplistTable">';
 		$persons = Person::get_toplist();
